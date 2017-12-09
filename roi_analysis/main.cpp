cf06a58c37b327f1881a9226a56fdbff4317841b
Lakshadeep <lakshadeep.naik@gmail.com>
2017-12-09 11:16:09 +0100

    * refactoring

--- roi_analysis/main.cpp
+++ roi_analysis/main.cpp
@@ -15,8 +15,6 @@ using namespace cv;
 using namespace std;
 using namespace cv::ximgproc::segmentation;
 
-Scalar getMSSIM( const Mat& i1, const Mat& i2);
-
 /// Generate grad_x and grad_y
 Mat grad_x, grad_y;
 Mat abs_grad_x, abs_grad_y;
@@ -37,74 +35,6 @@ string test_file = "0008.avi";
 string model_directory = "/media/lakshadeep/Common/MAS/Semester2/computer_vision/project_final/models/distant/";
 string experiment_results_directory = "/media/lakshadeep/Common/MAS/Semester2/computer_vision/project_final/experiment_results/roi/0008/";
 
-float correlation(Mat reg_prop, vector<string> model_list)
-{
-    float corr = 0.0, pt = 0, sum_T= 0, sum_R = 0, template_count = 0;
-    Scalar ssim=0;
-    Mat T,R;
-    int index = 0;
-    T = imread(model_list[0], 0);
-    resize(reg_prop, R, Size(T.size()), 0, 0);
-    //imshow("Rescaled ROI", R);
-    R.convertTo(R, CV_32F);
-    sum_R = sum(R)[0];
-
-    for(auto f = model_list.begin(); f!=model_list.end(); f++)
-    {
-        if(model_list[index].find(".png") != string::npos)
-        {
-            T = imread(model_list[index], 1);
-            //pt = countNonZero(T);
-            //T.convertTo(T, CV_32F);
-            //sum_T = sum(T)[0];
-            //corr += ((pt * sum(R.mul(T))[0]) - (sum_R * sum_T)) / (sqrt((pt * sum(R.mul(R))[0]) - pow(sum_R,2)) * sqrt((pt * sum(T.mul(T))[0]) - pow(sum_T,2)));
-            ssim += getMSSIM(R, T);
-            template_count ++;
-        }
-        index++;
-    }
-
-    return sum(ssim)[0]/ (template_count*3);
-}
-
-// from opencv documentation...
-Scalar getMSSIM( const Mat& i1, const Mat& i2)
-{
-    const double C1 = 6.5025, C2 = 58.5225;
-    /***************************** INITS **********************************/
-    int d = CV_32F;
-    Mat I1, I2;
-    i1.convertTo(I1, d);            // cannot calculate on one byte large values
-    i2.convertTo(I2, d);
-    Mat I2_2   = I2.mul(I2);        // I2^2
-    Mat I1_2   = I1.mul(I1);        // I1^2
-    Mat I1_I2  = I1.mul(I2);        // I1 * I2
-    /*************************** END INITS **********************************/
-    Mat mu1, mu2;                   // PRELIMINARY COMPUTING
-    GaussianBlur(I1, mu1, Size(11, 11), 1.5);
-    GaussianBlur(I2, mu2, Size(11, 11), 1.5);
-    Mat mu1_2   =   mu1.mul(mu1);
-    Mat mu2_2   =   mu2.mul(mu2);
-    Mat mu1_mu2 =   mu1.mul(mu2);
-    Mat sigma1_2, sigma2_2, sigma12;
-    GaussianBlur(I1_2, sigma1_2, Size(11, 11), 1.5);
-    sigma1_2 -= mu1_2;
-    GaussianBlur(I2_2, sigma2_2, Size(11, 11), 1.5);
-    sigma2_2 -= mu2_2;
-    GaussianBlur(I1_I2, sigma12, Size(11, 11), 1.5);
-    sigma12 -= mu1_mu2;
-    Mat t1, t2, t3;
-    t1 = 2 * mu1_mu2 + C1;
-    t2 = 2 * sigma12 + C2;
-    t3 = t1.mul(t2);                 // t3 = ((2*mu1_mu2 + C1).*(2*sigma12 + C2))
-    t1 = mu1_2 + mu2_2 + C1;
-    t2 = sigma1_2 + sigma2_2 + C2;
-    t1 = t1.mul(t2);                 // t1 =((mu1_2 + mu2_2 + C1).*(sigma1_2 + sigma2_2 + C2))
-    Mat ssim_map;
-    divide(t3, t1, ssim_map);        // ssim_map =  t3./t1;
-    Scalar mssim = mean(ssim_map);   // mssim = average of ssim map
-    return mssim;
-}
 
 int main(int, char**)
 {
@@ -134,11 +64,10 @@ int main(int, char**)
     closedir(dpdf);
 
     namedWindow("Original", CV_WINDOW_AUTOSIZE );
-    //namedWindow("Thresholded", CV_WINDOW_AUTOSIZE );
 
     cap >> frame; // get a new frame from camera
     resize(frame, frame, Size(), 0.5, 0.5);
-    //VideoWriter video("distant_car_detection_0008_1.avi",CV_FOURCC('M','J','P','G'),10, Size(frame.cols,frame.rows),true);
+
     int it_control = 0;
     int64 frame_no = 0;
 
@@ -207,10 +136,7 @@ int main(int, char**)
                 i = i + step_size;
             }
 
-            //video.write(frame);
-
             imshow("Original", frame);
-            //imshow("Thresholded", thresholded_grad);
             if(waitKey(10) >= 0) break;
             it_control ++;
         }
