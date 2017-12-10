f70cdf9ba2550c759359a91b77100056e7c27efc
Naresh Kumar Gurulingan <naresh.gurulingan@smail.inf.h-brs.de>
2017-12-10 14:05:41 +0100

    Bus core error fixed

--- Task2_svm/src/SupportVectorMachine.cpp
+++ Task2_svm/src/SupportVectorMachine.cpp
@@ -31,50 +31,47 @@ void SupportVectorMachine::extractHogFeatures(map<int, Mat> trainData)
 {
     Mat img;
     HOGDescriptor hog;
-    vector< float> descriptorsValues;
+    vector< float> descriptors;
     vector< Point> locations;
     img = trainData[1];
     //namedWindow("Image", CV_WINDOW_AUTOSIZE );
     //imshow("Image", imgGray);
     //waitKey(0);
-    hog.compute(img, descriptorsValues, Size(64, 64), Size(0, 0), locations);
+    hog.compute(img, descriptors, Size(64, 64), Size(0, 0), locations);
     cout<<"Hog values:..."<<endl;
-    cout<<descriptorsValues.size()<<endl;
+    cout<<descriptors.size()<<endl;
     cout<<locations.size()<<endl;
 }
 
 void SupportVectorMachine::trainSVM(map<int, Mat> trainData)
 {
 
-    int numImages = 2;
-    int imgArea = 64*64;
-    Mat trainingDataMat(numImages,imgArea,CV_32FC1);
-    Mat labelsMat(numImages, 1, CV_32SC1);
+    Mat trainingDataMat(4, trainData[0].rows * trainData[0].cols, CV_32FC1);
+    Mat labelsMat(4, 1, CV_32SC1);
 
-    int curCol = 0;
-    for(int imIndex = 0; imIndex < numImages; imIndex++) {
+    int curCol = 0, index = 0;
+    for(int imIndex = -2; imIndex < 2; imIndex++) {
         for (int rowIndex = 0; rowIndex < trainData[imIndex].rows; rowIndex++) {
             for (int colIndex = 0; colIndex < trainData[imIndex].cols; colIndex++) {
-                trainingDataMat.at<float>(imIndex,curCol++) = trainData[imIndex].at<uchar>(rowIndex,colIndex);
-                if (imIndex > 0) {
-                    labelsMat.at<float>(imIndex, 1) = 1;
-                }
-                else {
-                    labelsMat.at<float>(imIndex, 1) = -1;
+                trainingDataMat.at<float>(index,curCol++) = trainData[imIndex].at<uchar>(rowIndex,colIndex);
                 }
             }
+        if (imIndex >= 0) {
+            labelsMat.at<float>(index, 0) = 1;
         }
+        else {
+            labelsMat.at<float>(index, 0) = -1;
+        }
+        curCol = 0;
+        index++;
     }
-
-    cout<<"SVM Training data size:  "<<trainingDataMat.size()<<endl;
-    cout<<"SVM Label data size:  "<<labelsMat.size()<<endl;
 
     Ptr<SVM> svm = SVM::create();
     svm->setType(SVM::C_SVC);
     svm->setKernel(SVM::LINEAR);
     svm->setTermCriteria(TermCriteria(TermCriteria::MAX_ITER, 100, 1e-6));
     svm->train(trainingDataMat, ROW_SAMPLE, labelsMat);
-    svm->save("cars.yaml");
+    svm->save("cars.yml");
 }
 
 map<int, Mat> SupportVectorMachine::createTrainData()
